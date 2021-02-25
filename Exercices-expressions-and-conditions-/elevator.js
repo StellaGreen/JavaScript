7c5bc00f5333caaef668a6b439b24510f3e89c25
StellaGreen <vickysoler5@gmail.com>
2021-02-25 18:50:38 +0100

    changement const

--- Exercices-expressions-and-conditions-/elevator.js
+++ Exercices-expressions-and-conditions-/elevator.js
@@ -11,48 +11,49 @@ targetFloor: Pour l'étage où souhaite se rentre l'utilisateur
 let currentFloor = 5
 let targetFloor = [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
 const chalk = require('chalk');
+const log = console.log
 
 
 while (targetFloor >= -2 || targetFloor <= 7 !== currentFloor){
     switch (currentFloor){
         case -2 :{
-            console.log(chalk.blue('Etage n°-2'))
+            log(chalk.blue('Etage n°-2'))
             break
         }
         case -1 :{
-            console.log(chalk.pink('Etage n°-1'))
+            log(chalk.pink('Etage n°-1'))
             break
         }
         case 0 :{
-            console.log(chalk.white('RDC'))
+            log(chalk.white('RDC'))
             break
         }
         case 1 :{
-            console.log(chalk.green('Etage n°1'))
+            log(chalk.green('Etage n°1'))
             break
         }
         case 2 :{
-            console.log(chalk.red('Etage n°2'))
+            log(chalk.red('Etage n°2'))
             break
         }
         case 3 :{
-            console.log(chalk.yellow('Etage n°3'))
+            log(chalk.yellow('Etage n°3'))
             break
         }
         case 4 :{
-            console.log(chalk.grey('Etage n°4'))
+            log(chalk.grey('Etage n°4'))
             break
         }
         case 5 :{
-            console.log(chalk.purple('Etage n°5'))
+            log(chalk.purple('Etage n°5'))
             break
         }
         case 6 :{
-            console.log(chalk.black('Etage n°6'))
+            log(chalk.black('Etage n°6'))
             break
         }
         case 7 :{
-            console.log(chalk.magenta('Etage n°7'))
+            log(chalk.magenta('Etage n°7'))
             break
         }
         
