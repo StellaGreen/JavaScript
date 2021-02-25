019866ae95b645e91df194c1d85b56cadcee25a6
StellaGreen <vickysoler5@gmail.com>
2021-02-25 17:15:19 +0100

    exercices answer add

---
+++ Exercices expressions and conditions/vote.js
@@ -0,0 +1,15 @@
+/*let age = 17
+let canVote = false
+if (age >= 18) {
+  canVote = true
+} else {
+  canVote = false
+}
+if (canVote) {
+  console.log('You can vote')
+} else {
+  console.log('You can not vote')
+}*/
+let age =17
+let canvote = age >= 18 ? 'True\nYou can vote' : 'False\nYou can not vote'
+console.log(canvote)
