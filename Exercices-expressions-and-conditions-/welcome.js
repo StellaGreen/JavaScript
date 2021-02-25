10643bfc2506c0a39acd0cea95b6ae912e815197
StellaGreen <vickysoler5@gmail.com>
2021-02-25 18:57:15 +0100

    corrigé faute

--- Exercices-expressions-and-conditions-/welcome.js
+++ Exercices-expressions-and-conditions-/welcome.js
@@ -1,6 +1,6 @@
 /*Bonjour Bob, vous êtes entré sur le chan ---firstName---gender---
 Bonjour Alice, vous êtes entrée sur le chan*/
-let genger = 'female'
+let genger = 'femele'
 let firstname ='Alice'
-let msg = `Bonjour ${firstname}, vous êtes entré${genger === 'female' ? 'e' : ''} sur le chan.`
+let msg = `Bonjour ${firstname}, vous êtes entré${genger === 'femele' ? 'e' : ''} sur le chan.`
 console.log(msg)
