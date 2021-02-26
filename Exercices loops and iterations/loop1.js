abfd94026450d6df55f553eb2a684203b7163a95
StellaGreen <vickysoler5@gmail.com>
2021-02-26 15:49:32 +0100

    add answer

--- Exercices loops and iterations/loop1.js
+++ Exercices loops and iterations/loop1.js
@@ -1,8 +1,8 @@
-let names = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-while (names.length < 11 ) {
-    for (let i = 0; i < names.length; ++i) {
-          console.log(names[i])
+while (tab.length < 11 ) {
+    for (let i = 0; i < tab.length; ++i) {
+          console.log(tab[i])
         }
         break
       }
