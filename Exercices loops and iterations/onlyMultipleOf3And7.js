b88b89f21eda25211466538251f0278cf1663c5b
StellaGreen <vickysoler5@gmail.com>
2021-02-26 18:02:49 +0100

    add pb

---
+++ Exercices loops and iterations/onlyMultipleOf3And7.js
@@ -0,0 +1,5 @@
+/*
+En vous inspirant de l'un des exemple du cours d'aujourd'hui sur les boucles, 
+écrire un programme qui n'affiche que les multiples de 3 et 7 entre 1 et 1000. 
+L'opérateur % peut vous aider à trouver si un nombre est multiple d'un autre.
+*/
