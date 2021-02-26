d4e0aa36a043b0b1546a0582cf050cc379575447
StellaGreen <vickysoler5@gmail.com>
2021-02-26 15:46:55 +0100

    exercices en cours

---
+++ Exercices loops and iterations/loop1.js
@@ -0,0 +1,9 @@
+let names = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+while (names.length < 11 ) {
+    for (let i = 0; i < names.length; ++i) {
+          console.log(names[i])
+        }
+        break
+      }
+    
---
+++ Exercices loops and iterations/loop2.js
@@ -0,0 +1,16 @@
+let tab = ['a', 'b', 'c', 'd', 'e']
+
+do{
+    1 + 1 === 2
+} while (tab.length < 6 ) {
+    for (let i = 0; i < tab.length; ++i) {
+          console.log(tab[i])
+    }
+}
+/*let result = ''
+let i = 0
+do {
+  i = i + 1
+  result = result + i
+} while (i < 5)
+console.log(result)*/
