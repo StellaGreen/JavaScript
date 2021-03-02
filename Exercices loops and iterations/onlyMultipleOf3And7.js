b3e4f42ada1ad1a66dd6135fb7dfb7fa583c64fb
StellaGreen <vickysoler5@gmail.com>
2021-03-02 17:41:03 +0100

    add my exercice

--- Exercices loops and iterations/onlyMultipleOf3And7.js
+++ Exercices loops and iterations/onlyMultipleOf3And7.js
@@ -3,3 +3,10 @@ En vous inspirant de l'un des exemple du cours d'aujourd'hui sur les boucles,
 écrire un programme qui n'affiche que les multiples de 3 et 7 entre 1 et 1000. 
 L'opérateur % peut vous aider à trouver si un nombre est multiple d'un autre.
 */
+
+for(let i = 0; i<= 1000; ++i){
+    if(!(i % 3 === 0) && !(i % 7 === 0)){
+    continue
+    }
+    console.log(i)
+}
