654aaa631ba9103741b5fb43f9cd5024261184a9
StellaGreen <vickysoler5@gmail.com>
2021-03-03 18:52:59 +0100

    add

--- Exercices-functions/calc.js
+++ Exercices-functions/calc.js
@@ -4,5 +4,13 @@ const add = (nbr1, nbr2)=>{
 const sub = (nbr1, nbr2)=>{
     return nbr1 - nbr2
 }
+const mul = (nbr1, nbr2)=>{
+    return nbr1 * nbr2
+}
+const div = (nbr1, nbr2)=>{
+    return nbr1 / nbr2
+}
 console.log(add(2, 3))
 console.log(sub(15, 8))
+console.log(mul(2, 5))
+console.log(div(6, 2))
