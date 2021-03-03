52abab5d30debaa11b714a379634d3b03cf0c2de
StellaGreen <vickysoler5@gmail.com>
2021-03-03 19:03:48 +0100

    add chanement name of fonction

--- Exercices-functions/countWhells.js
+++ Exercices-functions/countWhells.js
@@ -1,5 +1,5 @@
-const countWheels = (mono, moto, car, lime)=>{
-    return mono + moto * 2 + car * 4 + lime * 6
+const countWheels = (nb_monocycle, nb_moto, nb_voiture, nb_limousine)=>{
+    return nb_monocycle + nb_moto * 2 + nb_voiture * 4 + nb_limousines * 6
 // j'annonce le nombre de roues existant sur chaque elements
 }
 console.log(countWheels(10, 20, 7, 2))
