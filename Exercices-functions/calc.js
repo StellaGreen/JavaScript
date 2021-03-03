93c503de11725a2f66520521519e05ac3d9cfd04
StellaGreen <vickysoler5@gmail.com>
2021-03-03 19:01:14 +0100

    add commentaires and answer

--- Exercices-functions/calc.js
+++ Exercices-functions/calc.js
@@ -1,16 +1,21 @@
 const add = (nbr1, nbr2)=>{
    return nbr1 + nbr2
+//je demande l'addition 
 }
 const sub = (nbr1, nbr2)=>{
     return nbr1 - nbr2
+//je demande la soustraction
 }
 const mul = (nbr1, nbr2)=>{
     return nbr1 * nbr2
+//je demande la multiplication
 }
 const div = (nbr1, nbr2)=>{
     return nbr1 / nbr2
+//je demande la division
 }
 console.log(add(2, 3))
 console.log(sub(15, 8))
 console.log(mul(2, 5))
 console.log(div(6, 2))
+//pas compris le dernier exercices de calc
---
+++ Exercices-functions/countWhells.js
@@ -0,0 +1,6 @@
+const countWheels = (mono, moto, car, lime)=>{
+    return mono + moto * 2 + car * 4 + lime * 6
+// j'annonce le nombre de roues existant sur chaque elements
+}
+console.log(countWheels(10, 20, 7, 2))
+console.log(countWheels(2, 3, 4, 5))
