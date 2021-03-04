da063bc76debd994343fca181ad55a0c9cfb1769
StellaGreen <vickysoler5@gmail.com>
2021-03-04 14:20:24 +0100

    add answer sup

--- Exercices-functions/calc.js
+++ Exercices-functions/calc.js
@@ -14,8 +14,26 @@ const div = (nbr1, nbr2)=>{
     return nbr1 / nbr2
 //je demande la division
 }
-console.log(add(2, 3))
-console.log(sub(15, 8))
-console.log(mul(2, 5))
-console.log(div(6, 2))
+const calc = (op, nbr1, nbr2) => {
+switch(op){
+    case '+': 
+        return add(nbr1, nbr2)
+    break
+    case '-':
+        return sub(nbr1, nbr2)
+    break
+    case '*':
+        return mul(nbr1, nbr2)
+    break
+    case '/':
+        return div(nbr1, nbr2)
+    break
+    default :
+    console.log('This is not a calcul')
+}
+}
+console.log(calc('+', 2, 3))
+console.log(calc('*', 15, 8))
+console.log(calc('-', 2, 5))
+console.log(calc('/', 6, 2))
 //pas compris le dernier exercices de calc
